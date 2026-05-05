a49034fa3edd046b194f066c0339b8d4876a8a33
savasanisujal1 <128718122+savasanisujal1@users.noreply.github.com>
2026-05-05 10:36:06 +0530

    implement advance product dashboard with live search

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@ Route::get('/', function () {
 
 
 Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products/{id}/restore', [ProductController::class, 'restore']);
+Route::post('/products/{id}/force-delete', [ProductController::class, 'forceDelete']);
+Route::post('/products/{id}/delete', [ProductController::class, 'destroy']);
+Route::get('/products/live-search', [ProductController::class, 'search']);
